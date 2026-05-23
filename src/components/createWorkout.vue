1b16128df614df342fc870ed709b4f153a73a8d2
trangdo <trangdo2026@gmail.com>
2026-05-23 16:17:15 +0200

    Typescript, field reset

--- src/components/createWorkout.vue
+++ src/components/createWorkout.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
 import {ref} from 'vue'
 type Workout = {
+  workoutName: string
   name: string
   reps: number
   sets: number
 }
-
-const workouts = ref([])
+const workouts = ref<Workout[]>([])
 const workoutNameField = ref('')
 const nameField = ref('')
 const repsField = ref(0)
@@ -14,10 +14,15 @@ const setField = ref(0)
 
 function save() {
   workouts.value.push({
+    workoutName: workoutNameField.value,
     name: nameField.value,
     reps: Number(repsField.value),
     sets: Number(setField.value)
   })
+
+  nameField.value = ''
+  repsField.value = null
+  setField.value = null
 }
 </script>
 
@@ -28,11 +33,12 @@ function save() {
   </h3>
   <div>
     <input v-model="nameField" placeholder="name of your exercise" type="text">
-    <input v-model="repsField" placeholder="reps" type="text">
-    <input v-model="setField" placeholder="sets" @keyup.enter="save()">
+    <input v-model="repsField" placeholder="reps" type="number">
+    <input v-model="setField" placeholder="sets"  type="number" @keyup.enter="save()">
     <button @click="save()">Save</button>
   </div>
   <div v-for="workout in workouts" :key="workout.name">
+    <h3>{{ workout.workoutName }}</h3>
     <h3>{{ workout.name }}</h3>
     <p>{{ workout.reps }} reps</p>
     <p>{{ workout.sets }} sets</p>
